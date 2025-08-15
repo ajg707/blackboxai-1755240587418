aa5591e76a3fc8bc0405fa757a9b64ae5658d288
ajg707 <52591474+ajg707@users.noreply.github.com>
2025-08-15 02:49:59 -0400

    Add .next/static/chunks/src_app_institutional-advancement_campaigns_[id]_page_tsx_d8b75cf9._.js 1755240599402

---
+++ .next/static/chunks/src_app_institutional-advancement_campaigns_[id]_page_tsx_d8b75cf9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b1da48d8._.js",
+  "static/chunks/node_modules_8746f117._.js"
+],
+    source: "dynamic"
+});
